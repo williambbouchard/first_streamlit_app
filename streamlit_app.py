4a1d08999b31237e33d6909ad56c5c3a49594ced
William BB <112104075+williambbouchard@users.noreply.github.com>
2022-08-26 09:46:01 -0400

    Update streamlit_app.py
    try/except

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import requests
 from urllib.error import URLError
 
 streamlit.title('🥣 My parents New Healthy Diner')
-
 streamlit.header('🥗 Breakfast Menu')
 streamlit.text('🐔 Omega 3 & Oatmeal')
 streamlit.text('🥑 Kale, Spinach & Smoothie')
@@ -17,25 +16,26 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?)
+  if not fruit_choice:
+        streamlit.error("Please select fruit")
+  else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+                                      streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 # streamlit.text(fruityvice_response)
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
